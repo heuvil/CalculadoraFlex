8e82e337e5253fab23366ba5e7231bfc269578af
heuvil <heuler@outlook.com>
2019-08-17 11:03:40 -0300

    Initial commit

---
+++ app/src/main/java/br/com/heuvil/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.heuvil.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.heuvil.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/heuvil/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.heuvil.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.heuvil.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

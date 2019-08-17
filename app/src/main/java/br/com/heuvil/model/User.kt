6d9913d54363280c34481a0817014f3e524c0150
heuvil <heuler@outlook.com>
2019-08-17 12:36:35 -0300

    segunda versao

---
+++ app/src/main/java/br/com/heuvil/model/User.kt
@@ -0,0 +1,8 @@
+package br.com.heuvil.model
+
+data class User(
+    val name: String = "",
+    val email: String = "",
+    val phone: String = ""
+)
+
--- app/src/main/java/br/com/heuvil/ui/signup/SignupActivity.kt
+++ app/src/main/java/br/com/heuvil/ui/signup/SignupActivity.kt
@@ -1,13 +1,66 @@
 package br.com.heuvil.ui.signup
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.heuvil.R
+import br.com.heuvil.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignupActivity : AppCompatActivity() {
+//class SignupActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_sign_up)
+//    }
+//}
 
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView (R.layout.activity_sign_up)
+        mAuth = FirebaseAuth.getInstance()
+        btCreate.setOnClickListener {
+            mAuth.createUserWithEmailAndPassword(
+                inputEmail.text.toString(),
+                inputPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    saveInRealTimeDatabase()
+                } else {
+                    Toast.makeText(
+                        this@SignUpActivity, it.exception?.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+
+private fun saveInRealTimeDatabase() {
+    val user = User(
+        inputName.text.toString(),
+        inputEmail.text.toString(),
+        inputPhone.text.toString())
+    FirebaseDatabase.getInstance().getReference("Users")
+        .child(FirebaseAuth.getInstance().currentUser!!.uid) .setValue(user)
+        .addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Usuário criado com sucesso",
+                    Toast.LENGTH_SHORT).show()
+                val returnIntent = Intent()
+                returnIntent.putExtra("email", inputEmail.text.toString())
+                setResult(RESULT_OK, returnIntent)
+                finish()
+            } else {
+                Toast.makeText(this, "Erro ao criar o usuário", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
